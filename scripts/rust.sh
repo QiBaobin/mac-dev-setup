151473e8b8ce54de806eebb63ee457cbd59bc693
Bob Qi <Baobin.qi@gmail.com>
2020-09-10 10:55:22 +0800

    use rust-analyzer instead of rls

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -6,7 +6,11 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # dev env
-rustup component add rls rust-analysis rust-src
+rustup component add rust-src
 
 # useful tools
 cargo install bat cross du-dust fd-find lsd procs ripgrep skim starship zoxide
+
+mkdir ~/code; cd ~/code && git clone --depth 1 https://github.com/rust-analyzer/rust-analyzer.git && cd rust-analyzer && cargo xtask install --server
+
+exit 0
