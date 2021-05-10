8c04dcdeda78f0e003cd25d68258024b27cfa45b
Bob Qi <Baobin.Qi@gmail.com>
2021-05-10 11:31:22 +0800

    use packer & plugins.lua

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -15,55 +15,6 @@ vim.o.completeopt = 'menuone,noinsert,noselect'
 api.nvim_set_keymap('c', '<c-n>',  '<down>', { noremap = true })
 api.nvim_set_keymap('c', '<c-p>',  '<up>', { noremap = true })
 
-vim.cmd([[
-call plug#begin('~/.vim/plugged')
-
-Plug 'tpope/vim-sensible'
-Plug 'tpope/vim-sleuth'
-Plug 'tpope/vim-fugitive'
-  nmap   <Leader>g :Git<CR>gg<c-n>
-  nnoremap <Leader>d :Gdiffsplit<CR>
-Plug 'tpope/vim-commentary'
-  map  gc  <Plug>Commentary
-  nmap gcc <Plug>CommentaryLine
-Plug 'tpope/vim-unimpaired'
-Plug 'tpope/vim-surround'
-Plug 'tpope/vim-dispatch'
-  nnoremap <Leader>: :Dispatch<space>
-  nnoremap <Leader>& :Dispatch!<space>
-Plug 'justinmk/vim-sneak'
-  let g:sneak#label = 1
-
-Plug 'udalov/kotlin-vim'
-Plug 'QiBaobin/vim-gradle'
-
-Plug 'autozimu/LanguageClient-neovim', {  'branch': 'next',  'do': 'bash install.sh',  }
-  function LC_maps()
-    if has_key(g:LanguageClient_serverCommands, &filetype)
-      " note that if you are using Plug mapping you should not use `noremap` mappings.
-      nmap <buffer> <LocalLeader>m <Plug>(lcn-menu)
-      nmap <buffer> <LocalLeader>r <Plug>(lcn-code-lens-action)
-      nmap <buffer> <silent> gd <Plug>(lcn-definition)
-      setlocal completefunc=LanguageClient#complete
-      setlocal formatexpr=LanguageClient#textDocument_rangeFormatting_sync()
-    endif
-  endfunction
-  autocmd FileType * call LC_maps()
-
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-Plug 'AndrewRadev/splitjoin.vim'
-
-Plug 'vim-airline/vim-airline'
-  let g:airline#extensions#tabline#enabled = 1
-Plug 'morhetz/gruvbox'
-Plug 'airblade/vim-gitgutter'
-Plug 'scrooloose/nerdtree'
-
-" Initialize plugin system
-call plug#end()
-]], false)
-vim.cmd('colorscheme gruvbox')
-
 local abbrev_file="~/.config/nvim/abbrev.vim"
 if vim.fn.findfile(abbrev_file) ~= '' then
     vim.cmd('source ' .. abbrev_file)
@@ -93,3 +44,5 @@ function Grep(bang, args, prg)
 	vim.cmd('copen')
     end
 end
+
+require('plugins')
---
+++ configs/nvim/lua/plugins.lua
@@ -0,0 +1,51 @@
+return require('packer').startup(function()
+
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-sleuth'
+  use { 'tpope/vim-fugitive', config = function() 
+    vim.api.nvim_set_keymap('', '<Leader>g',  ':Git<CR>gg<c-n>', {})
+    vim.api.nvim_set_keymap('n', '<Leader>d',  ':Gdiffsplit<CR>', {})
+  end}
+  use { 'tpope/vim-commentary', config = function()
+    vim.api.nvim_set_keymap('', '<Leader>c',  '<Plug>Commentary', {})
+    vim.api.nvim_set_keymap('n', '<Leader>cc',  '<Plug>CommentaryLine', {})
+  end}
+  use 'tpope/vim-unimpaired'
+  use 'tpope/vim-surround'
+  use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}, config = function()
+    vim.api.nvim_set_keymap('n', '<Leader>:',  ':Dispatch<space>', {})
+    vim.api.nvim_set_keymap('n', '<Leader>&',  ':Dispatch!<space>', {})
+  end}
+
+  use 'udalov/kotlin-vim'
+  use 'QiBaobin/vim-gradle'
+
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'autozimu/LanguageClient-neovim',   branch = 'next',  run = 'bash install.sh' , config = function()
+    vim.cmd([[
+    let g:LanguageClient_serverCommands = {  'rust': ['rust-analyzer'],  'python': ['pyls'],  }
+    function LC_maps()
+      if has_key(g:LanguageClient_serverCommands, &filetype)
+        nmap <buffer> <LocalLeader>m <Plug>(lcn-menu)
+        nmap <buffer> <LocalLeader>r <Plug>(lcn-code-lens-action)
+        nmap <buffer> <silent> gd <Plug>(lcn-definition)
+        setlocal completefunc=LanguageClient#complete
+        setlocal formatexpr=LanguageClient#textDocument_rangeFormatting_sync()
+      endif
+    endfunction
+    autocmd FileType * call LC_maps()
+    ]])
+  end}
+
+  use 'AndrewRadev/splitjoin.vim'
+  use 'wellle/targets.vim'
+  use 'airblade/vim-gitgutter'
+
+  use { 'vim-airline/vim-airline', config = 'vim.cmd([[let g:airline#extensions#tabline#enabled = 1]])' }
+  use { 'morhetz/gruvbox', config = 'vim.cmd([[colorscheme gruvbox]])' }
+  use 'scrooloose/nerdtree'
+
+end)
