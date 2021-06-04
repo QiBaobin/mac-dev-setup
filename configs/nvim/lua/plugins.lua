1af04413c1ce02700e91d776a5995b3646bd220d
Bob Qi <Baobin.Qi@gmail.com>
2021-06-04 15:32:32 +0800

    reduce vim plugins

--- configs/nvim/lua/plugins.lua
+++ configs/nvim/lua/plugins.lua
@@ -17,52 +17,20 @@ function()
   use 'tpope/vim-unimpaired'
   use 'tpope/vim-surround'
 
-  use 'QiBaobin/vim-gradle'
-  use 'sheerun/vim-polyglot'
-
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'autozimu/LanguageClient-neovim',   branch = 'next',  run = 'bash install.sh' , config = function()
-    vim.cmd([[
-    let g:LanguageClient_serverCommands = {  'rust': ['rust-analyzer'],  'python': ['pyls'],  }
-    let $RUST_BACKTRACE = 1
-    let g:LanguageClient_loggingLevel = 'WARN'
-    let g:LanguageClient_virtualTextPrefix = ''
-    let g:LanguageClient_loggingFile =  expand('~/.local/share/nvim/LanguageClient.log')
-    let g:LanguageClient_serverStderr = expand('~/.local/share/nvim/LanguageServer.log')
-
-    function! LC_maps()
-      if has_key(g:LanguageClient_serverCommands, &filetype)
-        nmap <buffer> <LocalLeader>m <Plug>(lcn-menu)
-        nmap <buffer> <LocalLeader>r <Plug>(lcn-code-lens-action)
-        nmap <buffer> <LocalLeader>e <Plug>(lcn-explain-error)
-        nmap <buffer> <silent> gd <Plug>(lcn-definition)
-        setlocal completefunc=LanguageClient#complete
-        setlocal formatexpr=LanguageClient#textDocument_rangeFormatting_sync()
-      endif
-    endfunction
-    autocmd FileType * call LC_maps()
-    ]])
-  end }
-
   use 'AndrewRadev/splitjoin.vim'
   use 'wellle/targets.vim'
-  use 'airblade/vim-gitgutter'
+  use "tversteeg/registers.nvim"
 
-  use { 'vim-airline/vim-airline', config = 'vim.cmd([[let g:airline#extensions#tabline#enabled = 0]])' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'neovim/nvim-lspconfig' }
+  use {'ray-x/navigator.lua', requires = {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}, config = function() require'navigator'.setup() end }
+
+  -- use { 'vim-airline/vim-airline', config = 'vim.cmd([[let g:airline#extensions#tabline#enabled = 0]])' }
   use { 'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons', config = function()
     vim.api.nvim_set_keymap('n', '<Leader>b',  ':BufferLinePick<CR>', { noremap = true })
-    require("bufferline").setup{
-      options = {
-        numbers = "both",
-        number_style = "superscript",
-        diagnostics = "nvim_lsp",
-      }
-    }
+    require("bufferline").setup{}
   end }
   use { 'morhetz/gruvbox', config = 'vim.cmd([[colorscheme gruvbox]])' }
-  use { 'scrooloose/nerdtree', opt = ture, cmd = { 'NERDTree' } }
-
-  use "tversteeg/registers.nvim"
 end,
 config = {
   git = {
