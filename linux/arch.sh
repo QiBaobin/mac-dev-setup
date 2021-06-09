de807b715e55f76e5160fb60d663a1dd2f9dbc86
Bob Qi <baobin.qi@gmail.com>
2021-06-09 23:03:31 +0800

    test linux

--- linux/arch.sh
+++ linux/arch.sh
@@ -1,16 +1,12 @@
 # install packages
-sudo pacman -S herbstluftwm rofi alacritty ttf-fira-code dzen2 ctags rustup rust-analyzer zsh zsh-autosuggestions zsh-syntax-highlighting starship firefox wqy-microhei skim
+sudo pacman -S herbstluftwm rofi alacritty ttf-fira-code dzen2 ctags rustup rust-analyzer zsh zsh-autosuggestions zsh-syntax-highlighting starship bat ripgrep fd firefox wqy-microhei skim git xclip base-devel sudo cmake unzip ninja tree-sitter python-pip
 
-# setup neovim
-old_pwd="$PWD"
-mkdir ~/.cache; cd ~/.cache && git clone https://aur.archlinux.org/neovim-nightly-bin.git; cd neovim-nightly-bin; makepkg -i
-cd "$old_pwd"
-
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config; cp -Rv ../config/nvim ~/.config/
-# use fastgit instead github if installing plugins too slow
-# sed -i -e 's/github.com/hub.fastgit.org/g' "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
 
+mkdir ~/git ~/.local; git clone --depth 1 https://github.com/neovim/neovim.git ~/git/neovim; cd ~/git/neovim && make CMAKE_INSTALL_PREFIX=~/.local/ install && cd -
+git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # all dot files
-cp -Rv dots/* ~/
+cp -Rv dots/.* ~/
+~/.local/bin/nvim +PackerInstall +PackerCompile +qa
+pip install neovim-remote
--- linux/dots/.zshenv
+++ linux/dots/.zshenv
@@ -1,4 +1,4 @@
 export LC_ALL=en_US.UTF-8
 export EDITOR='nvim'
 export TERMINAL='alacritty'
-export PATH="$HOME/bin:$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
