584cf5f1877809fde650a1d554b44f0d7ecc7d42
Bob Qi <Baobin.Qi@gmail.com>
2022-02-15 15:32:14 +0800

    correct variable name

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,9 +1,9 @@
 
 #!/usr/bin/env bash
 
-if [ -z "$helix_load" ]; then
+if [ -z "$alacritty_load" ]; then
     mkdir ~/code; git clone https://github.com/alacritty/alacritty.git
     cd ~/code/alacritty; make app; cd -
 
-    helix_load=1
+    alacritty_load=1
 fi
