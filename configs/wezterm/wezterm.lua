4c9d91ed5dfcdaeabab24b1daf37dd9c3ca59994
Bob <QiBaobin@users.noreply.github.com>
2023-03-20 20:45:28 +0800

    Update wezterm.lua
    use default window decoration

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -8,7 +8,6 @@ config.window_background_opacity = 0.7
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font_with_fallback {"Hack Nerd Font Mono", "Fira Code", "Monaco", "Inconsolata"}
 config.font_size = 16.0
-config.window_decorations = "NONE"
 config.text_background_opacity = 1.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
 
