3484879c4a55429160157743114091d673f4d58b
Bob <QiBaobin@users.noreply.github.com>
2023-03-20 20:34:19 +0800

    Update wezterm.lua
    config builder

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,11 +1,15 @@
 local wezterm = require 'wezterm';
 
-return {
-    hide_tab_bar_if_only_one_tab = true,
-    font = wezterm.font_with_fallback({"Hack Nerd Font Mono", "Fira Code", "Monaco", "Inconsolata"}),
-    font_size = 16.0,
-    window_background_opacity = 0.5,
-    window_decorations = "NONE",
-    text_background_opacity = 1.0,
-    window_padding = { left = 0, right = 0, top = 50, bottom = 0, },
-}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.window_background_opacity = 0.7
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font_with_fallback {"Hack Nerd Font Mono", "Fira Code", "Monaco", "Inconsolata"}
+config.font_size = 16.0
+config.window_decorations = "NONE"
+config.text_background_opacity = 1.0
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
+
+return config
