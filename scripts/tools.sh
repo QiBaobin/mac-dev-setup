35f77b53ebfad943b2c09cc844ba62916886e29f
Bob <QiBaobin@users.noreply.github.com>
2023-04-07 15:24:10 +0800

    Update tools.sh

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -4,8 +4,9 @@ if [ -z "$tools_load" ]; then
 
     source ./brew.sh
 
-    brew install aspell pandoc openssl git firefox aspell tmux
+    brew install aspell pandoc openssl git firefox aspell
     brew install --HEAD kakoune
+    brew install --HEAD wezterm
 
     tools_load=1
 fi
