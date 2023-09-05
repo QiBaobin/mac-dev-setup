c966e3a1a46858972c2f2120afbb6836bc27fe4a
Bob <QiBaobin@users.noreply.github.com>
2023-09-05 11:14:30 +0800

    Update wezterm.lua
    split pane

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ config.command_palette_font_size = 16.0
 config.window_background_opacity = 0.9
 config.text_background_opacity = 1.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
-config.color_scheme = 'Darcula (base16)'
+config.color_scheme = 'DanQin (base16)'
 
 config.keys = {
   {
@@ -26,55 +26,41 @@ config.keys = {
     },
   },
   {
+    key = 'n',
+    mods = 'CTRL|SHIFT',
+    action = act.ActivateKeyTable {
+      name = 'spawn_pane',
+    },
+  },
+  {
     key = 'Space',
     mods = 'CTRL',
-    action = act.ActivateKeyTable {
-      name = 'activate_pane',
-      one_shot = false,
-      timeout_milliseconds = 1000,
+    action = act.PaneSelect,
+  },
+  {
+    key = 'Space',
+    mods = 'CTRL|ALT',
+    action = act.PaneSelect {
+        mode = 'SwapWithActive',
     },
   },
 }
 
 config.key_tables = {
-  -- Defines the keys that are active in our resize-pane mode.
-  -- Since we're likely to want to make multiple adjustments,
-  -- we made the activation one_shot=false. We therefore need
-  -- to define a key assignment for getting out of this mode.
-  -- 'resize_pane' here corresponds to the name="resize_pane" in
-  -- the key assignments above.
   resize_pane = {
-    { key = 'LeftArrow', action = act.AdjustPaneSize { 'Left', 1 } },
-    { key = 'h', action = act.AdjustPaneSize { 'Left', 1 } },
-
-    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
-    { key = 'l', action = act.AdjustPaneSize { 'Right', 1 } },
-
-    { key = 'UpArrow', action = act.AdjustPaneSize { 'Up', 1 } },
-    { key = 'k', action = act.AdjustPaneSize { 'Up', 1 } },
-
-    { key = 'DownArrow', action = act.AdjustPaneSize { 'Down', 1 } },
-    { key = 'j', action = act.AdjustPaneSize { 'Down', 1 } },
-
-    -- Cancel the mode by pressing escape
+    { key = 'h', action = act.AdjustPaneSize { 'Left', 5 } },
+    { key = 'l', action = act.AdjustPaneSize { 'Right', 5 } },
+    { key = 'k', action = act.AdjustPaneSize { 'Up', 5 } },
+    { key = 'j', action = act.AdjustPaneSize { 'Down', 5 } },
     { key = 'Escape', action = 'PopKeyTable' },
   },
 
-  -- Defines the keys that are active in our activate-pane mode.
-  -- 'activate_pane' here corresponds to the name="activate_pane" in
-  -- the key assignments above.
-  activate_pane = {
-    { key = 'LeftArrow', action = act.ActivatePaneDirection 'Left' },
-    { key = 'h', action = act.ActivatePaneDirection 'Left' },
-
-    { key = 'RightArrow', action = act.ActivatePaneDirection 'Right' },
-    { key = 'l', action = act.ActivatePaneDirection 'Right' },
-
-    { key = 'UpArrow', action = act.ActivatePaneDirection 'Up' },
-    { key = 'k', action = act.ActivatePaneDirection 'Up' },
-
-    { key = 'DownArrow', action = act.ActivatePaneDirection 'Down' },
-    { key = 'j', action = act.ActivatePaneDirection 'Down' },
+  spawn_pane = {
+    { key = 'h', action = act.SplitPane { direction = 'Left', } },
+    { key = 'l', action = act.SplitPane { direction = 'Right', } },
+    { key = 'k', action = act.SplitPane { direction = 'Up', } },
+    { key = 'j', action = act.SplitPane { direction = 'Down', } },
+    { key = 'Escape', action = 'PopKeyTable' },
   },
 }
 return config
