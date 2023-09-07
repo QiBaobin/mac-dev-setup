f9c99326a232e1707d936c6b9d122a4dae7fba7c
Bob <QiBaobin@users.noreply.github.com>
2023-09-07 15:09:35 +0800

    Update wezterm.lua
    add trigger-editor
    random color_scheme

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -13,9 +13,60 @@ config.command_palette_font_size = 16.0
 config.window_background_opacity = 0.9
 config.text_background_opacity = 1.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
-config.color_scheme = 'DanQin (base16)'
+
+-- config.color_scheme = "Azu (Gogh)"
+-- The set of schemes that we like and want to put in our rotation
+local schemes = {}
+for name, scheme in pairs(wezterm.get_builtin_color_schemes()) do
+  table.insert(schemes, name)
+end
+wezterm.on('window-config-reloaded', function(window, pane)
+  -- If there are no overrides, this is our first time seeing
+  -- this window, so we can pick a random scheme.
+  if not window:get_config_overrides() then
+    -- Pick a random scheme name
+    local scheme = schemes[math.random(#schemes)]
+    window:set_config_overrides {
+      color_scheme = scheme,
+    }
+    window:maximize()
+  end
+end)
+
+wezterm.on('trigger-editor', function(window, pane)
+  local text = pane:get_logical_lines_as_text(window:effective_config().scrollback_lines)
+
+  -- Create a temporary file to pass to kak
+  local name = os.tmpname()
+  local f = io.open(name, 'w+')
+  f:write(text)
+  f:flush()
+  f:close()
+
+  -- Open a new window running editorand tell it to open the file
+  window:perform_action(
+    act.SpawnCommandInNewWindow {
+      args = { os.getenv 'SHELL', '-i', '-c', 'kak ' .. wezterm.shell_quote_arg(name) },
+    },
+    pane
+  )
+
+  -- Wait "enough" time for kak to read the file before we remove it.
+  -- The window creation and process spawn are asynchronous wrt. running
+  -- this script and are not awaitable, so we just pick a number.
+  --
+  -- Note: We don't strictly need to remove this file, but it is nice
+  -- to avoid cluttering up the temporary directory.
+  wezterm.sleep_ms(1000)
+  os.remove(name)
+end)
 
 config.keys = {
+  {
+    key = 'e',
+    mods = 'CTRL|SHIFT',
+    action = act.EmitEvent 'trigger-editor',
+  },
   {
     key = 'r',
     mods = 'CTRL|SHIFT',
