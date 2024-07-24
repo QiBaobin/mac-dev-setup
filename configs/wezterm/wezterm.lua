d192bc5d04e3c52f8ce9e28625af40b70b8cd60c
Bob <QiBaobin@users.noreply.github.com>
2024-07-24 09:35:57 +0800

    Update wezterm.lua
    respect appearance

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ if f ~= nil then
     }
 end
 config.hide_tab_bar_if_only_one_tab = true
-local fonts = { "Hack Nerd Font Mono", "Input Mono", "Fira Code", "Inconsolata", "Monaco", "Monaspace Neon" }
+local fonts = {"Cascadia Code", "Hack Nerd Font Mono", "Input Mono", "Fira Code", "Inconsolata", "Monaco", "Monaspace Neon" }
 config.font = wezterm.font_with_fallback(fonts)
 config.font_size = 16.0
 config.command_palette_font_size = 16.0
@@ -24,12 +24,38 @@ config.window_background_opacity = 0.9
 config.text_background_opacity = 1.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
 
--- config.color_scheme = "Azu (Gogh)"
--- The set of schemes that we like and want to put in our rotation
-local schemes = {}
-for name, scheme in pairs(wezterm.get_builtin_color_schemes()) do
-  table.insert(schemes, name)
+---cycle through builtin dark schemes in dark mode,
+---and through light schemes in light mode
+local function themeCycler(window, _)
+    local allSchemes = wezterm.color.get_builtin_schemes()
+    local isDark = wezterm.gui and wezterm.gui.get_appearance():find("Dark") or false
+    local currentScheme = window:effective_config().color_scheme
+    local darkSchemes = {}
+    local lightSchemes = {}
+
+    for name, scheme in pairs(allSchemes) do
+        if name ~= currentScheme and scheme.background then
+            local bg = wezterm.color.parse(scheme.background) -- parse into a color object
+            ---@diagnostic disable-next-line: unused-local
+            local h, s, l, a = bg:hsla() -- and extract HSLA information
+            if l < 0.4 then
+                table.insert(darkSchemes, name)
+            else
+                table.insert(lightSchemes, name)
+            end
+        end
+    end
+
+    local schemesToSearch = isDark and darkSchemes or lightSchemes
+    local theme = schemesToSearch[math.random(#schemesToSearch)]
+    local overrides = window:get_config_overrides() or {}
+    overrides.color_scheme = theme
+    table.insert(fonts, 1, table.remove(fonts, math.random(#fonts - 1) + 1))
+    overrides.font = wezterm.font_with_fallback(fonts)
+    wezterm.log_info("Switched to: " .. theme)
+    window:set_config_overrides(overrides)
 end
+
 wezterm.on('window-config-reloaded', function(window, pane)
   -- If there are no overrides, this is our first time seeing
   -- this window, so we can pick a random scheme.
@@ -39,15 +65,7 @@ wezterm.on('window-config-reloaded', function(window, pane)
   end
 end)
 
-wezterm.on('next-scheme', function(window, pane)
-    -- Pick a random scheme name
-    local scheme = schemes[math.random(#schemes)]
-    table.insert(fonts, 1, table.remove(fonts, math.random(#fonts - 1) + 1))
-    window:set_config_overrides {
-        color_scheme = scheme,
-        font = wezterm.font_with_fallback(fonts)
-    }
-end)
+wezterm.on('next-scheme', themeCycler)
 
 wezterm.on('trigger-editor', function(window, pane)
   local text = pane:get_logical_lines_as_text(window:effective_config().scrollback_lines)
@@ -78,61 +96,61 @@ wezterm.on('trigger-editor', function(window, pane)
 end)
 
 config.keys = {
-  {
-    key = 'e',
-    mods = 'CTRL|SHIFT',
-    action = act.EmitEvent 'trigger-editor',
-  },
+  -- {
+  --   key = 'e',
+  --   mods = 'CTRL|SHIFT',
+  --   action = act.EmitEvent 'trigger-editor',
+  -- },
   {
     key = 'c',
     mods = 'CTRL|SHIFT',
     action = act.EmitEvent 'next-scheme',
   },
-  {
-    key = 'r',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateKeyTable {
-      name = 'resize_pane',
-      one_shot = false,
-      timeout_milliseconds = 2000,
-    },
-  },
-  {
-    key = 'n',
-    mods = 'CTRL|SHIFT',
-    action = act.ActivateKeyTable {
-      name = 'spawn_pane',
-    },
-  },
+  -- {
+  --   key = 'r',
+  --   mods = 'CTRL|SHIFT',
+  --   action = act.ActivateKeyTable {
+  --     name = 'resize_pane',
+  --     one_shot = false,
+  --     timeout_milliseconds = 2000,
+  --   },
+  -- },
+  -- {
+  --   key = 'n',
+  --   mods = 'CTRL|SHIFT',
+  --   action = act.ActivateKeyTable {
+  --     name = 'spawn_pane',
+  --   },
+  -- },
   {
     key = 'Space',
     mods = 'CTRL',
     action = act.PaneSelect,
   },
-  {
-    key = 'Space',
-    mods = 'CTRL|ALT',
-    action = act.PaneSelect {
-        mode = 'SwapWithActive',
-    },
-  },
+  -- {
+  --   key = 'Space',
+  --   mods = 'CTRL|ALT',
+  --   action = act.PaneSelect {
+  --       mode = 'SwapWithActive',
+  --   },
+  -- },
 }
 
-config.key_tables = {
-  resize_pane = {
-    { key = 'h', action = act.AdjustPaneSize { 'Left', 5 } },
-    { key = 'l', action = act.AdjustPaneSize { 'Right', 5 } },
-    { key = 'k', action = act.AdjustPaneSize { 'Up', 5 } },
-    { key = 'j', action = act.AdjustPaneSize { 'Down', 5 } },
-    { key = 'Escape', action = 'PopKeyTable' },
-  },
+-- config.key_tables = {
+--   resize_pane = {
+--     { key = 'h', action = act.AdjustPaneSize { 'Left', 5 } },
+--     { key = 'l', action = act.AdjustPaneSize { 'Right', 5 } },
+--     { key = 'k', action = act.AdjustPaneSize { 'Up', 5 } },
+--     { key = 'j', action = act.AdjustPaneSize { 'Down', 5 } },
+--     { key = 'Escape', action = 'PopKeyTable' },
+--   },
 
-  spawn_pane = {
-    { key = 'h', action = act.SplitPane { direction = 'Left', } },
-    { key = 'l', action = act.SplitPane { direction = 'Right', } },
-    { key = 'k', action = act.SplitPane { direction = 'Up', } },
-    { key = 'j', action = act.SplitPane { direction = 'Down', } },
-    { key = 'Escape', action = 'PopKeyTable' },
-  },
-}
+--   spawn_pane = {
+--     { key = 'h', action = act.SplitPane { direction = 'Left', } },
+--     { key = 'l', action = act.SplitPane { direction = 'Right', } },
+--     { key = 'k', action = act.SplitPane { direction = 'Up', } },
+--     { key = 'j', action = act.SplitPane { direction = 'Down', } },
+--     { key = 'Escape', action = 'PopKeyTable' },
+--   },
+-- }
 return config
