aead372066a36f8454f3dd6aa80bb0d51497d9aa
Bob <QiBaobin@users.noreply.github.com>
2024-07-26 10:22:31 +0800

    Delete scripts directory

--- scripts/alacritty.sh
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/env bash
-
-if [ -z "$alacritty_load" ]; then
-    mkdir ~/code; git clone https://github.com/alacritty/alacritty.git
-    cd ~/code/alacritty; make app; cd -
-
-    alacritty_load=1
-fi
