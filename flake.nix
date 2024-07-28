5126ba94de60bbec9005a261ee3faf474cef178f
Bob Qi <Baobin.Qi@gmail.com>
2024-07-28 10:35:13 +0800

    refactor flake

--- flake.nix
+++ flake.nix
@@ -6,25 +6,25 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        packages.default = pkgs.writeScriptBin "swithHome" ''
-        #!/bin/sh
+      {
+        apps.default = let
+          pkgs = import nixpkgs { inherit system; };
+          swithHome = pkgs.writeScriptBin "swithHome" ''
+            #!/bin/sh
 
-        echo '{ ... }:  { home = { username = "'$USER'"; homeDirectory = "'$HOME'"; }; }' > home-manager/user.nix
-        echo '{ ... }:  { programs.zsh.sessionVariables.no_proxy = "'$no_proxy'"; programs.zsh.sessionVariables.http_proxy = "'$http_proxy'"; programs.zsh.sessionVariables.https_proxy = "'$https_proxy'"; }' > home-manager/proxy.nix
-        nix run home-manager/master -- switch --flake ./home-manager#bob -b backup
-        echo > home-manager/user.nix
-        echo > home-manager/proxy.nix
+            echo '{ ... }:  { home = { username = "'$USER'"; homeDirectory = "'$HOME'"; }; }' > home-manager/user.nix
+            echo '{ ... }:  { programs.zsh.sessionVariables.no_proxy = "'$no_proxy'"; programs.zsh.sessionVariables.http_proxy = "'$http_proxy'"; programs.zsh.sessionVariables.https_proxy = "'$https_proxy'"; }' > home-manager/proxy.nix
+            nix run home-manager/master -- switch --flake ./home-manager#bob -b backup
+            echo > home-manager/user.nix
+            echo > home-manager/proxy.nix
 
-        cp -v configs/kak/kakrc $HOME/.config/kak/realrc
-        cp -Rv configs/skhd $HOME/.config/
-        '';
-        apps.default = {
+            cp -v configs/kak/kakrc $HOME/.config/kak/realrc
+            cp -Rv configs/skhd $HOME/.config/
+            '';
+        in {
           type = "app";
-          program = "${self.packages.${system}.default}/bin/swithHome";
+          program = "${swithHome}/bin/swithHome";
         };
       }
     );
-}
+    }
