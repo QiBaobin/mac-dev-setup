4a6029614e442d2d9771d12f02723d40144d0864
Bob Qi <Baobin.Qi@gmail.com>
2024-08-27 11:34:11 +0800

    hardtime.nvim noice.nvim

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -746,19 +746,20 @@ require('lazy').setup({
         'c',
         'diff',
         'html',
+        'java',
+        'kotlin',
         'lua',
         'luadoc',
         'markdown',
         'markdown_inline',
+        'nix',
+        'python',
         'query',
+        'regex',
+        'rust',
         'vim',
         'vimdoc',
-        'nix',
-        'kotlin',
-        'java',
         'zig',
-        'rust',
-        'python',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -789,6 +790,47 @@ require('lazy').setup({
   "tpope/vim-fugitive",
   "Exafunction/codeium.vim",
   "tpope/vim-dispatch",
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+    config = function(_, opts) require("hardtime").setup(opts) end
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+      },
+    config = function(_, opts)
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
