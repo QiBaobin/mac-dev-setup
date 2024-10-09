52bdc532a42e24645967d8c4f9590e07cab9da9f
Bob Qi <Baobin.Qi@gmail.com>
2024-10-09 19:35:44 +0800

    disable mini.ai

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -24,23 +24,10 @@ return { -- Highlight, edit, and navigate code
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn", -- set to `false` to disable one of the mappings
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },
+    highlight = { enable = true, },
+    indent = { enable = true, },
+    incremental_selection = { enable = true, },
+    textobjects = { enable = true },
   },
   config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
