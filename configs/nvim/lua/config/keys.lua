e6d6420082823f5cec13d275927e94d897afc62d
Bob Qi <Baobin.Qi@gmail.com>
2024-10-11 19:07:24 +0800

    use one key to exit terminal mode

--- configs/nvim/lua/config/keys.lua
+++ configs/nvim/lua/config/keys.lua
@@ -34,4 +34,4 @@ vim.keymap.set('c', '<C-k>', '<Up>')
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<C-]>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
