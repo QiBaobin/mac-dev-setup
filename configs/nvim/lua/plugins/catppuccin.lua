432324e58ac4facc0561647e9db07d4cd4770267
Bob Qi <Baobin.Qi@gmail.com>
2024-10-15 16:21:42 +0800

    small UI changes

--- configs/nvim/lua/plugins/catppuccin.lua
+++ configs/nvim/lua/plugins/catppuccin.lua
@@ -2,7 +2,12 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  opts = { transparent_background = true },
+  opts = {
+    transparent_background = true,
+    default_integrations = {
+      treesitter = true,
+    },
+  },
   config = function(_, opts)
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme "catppuccin"
--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -24,7 +24,10 @@ return { -- Highlight, edit, and navigate code
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
-    highlight = { enable = true, },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
     indent = { enable = true, },
     incremental_selection = { enable = true, },
     textobjects = { enable = true },
